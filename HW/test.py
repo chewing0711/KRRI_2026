d5d5632598f1ad69288e1c49b77af41f7d488f62
chewing0711 <parkmingyu711@gmail.com>
2026-08-18 20:24:17 +0900

    add: add function for data collect by can id

--- HW/test.py
+++ HW/test.py
@@ -17,15 +17,17 @@ try:
         if msg is None:
             continue
 
-        recived_time, can_id, dlc, data = can_parser.parser(msg)
+        recived_time, can_id, data = can_parser.parser(msg)
 
         if start_time is None :
             start_time = recived_time
 
         if can_id is not None:
-            relative_time = round(msg_time - first_time, 5)
-            dataset = collect_data(relative_time, can_id, data)
-        
+            relative_time = round(recived_time - start_time, 5)
+            dataset = can_parser.collect_data(relative_time, can_id, data)
+
+            if dataset is not None:
+                print(f"here, {dataset}")
 
 except KeyboardInterrupt:
     print("Exit.")
